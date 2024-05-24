e871584397b9de1925f78ef9180e3683bdd7d7a4
simon.weckler <simon.wecker@check24.de>
2024-05-24 19:10:38 +0200

    add storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,14 @@
+import "~/index.css";
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
