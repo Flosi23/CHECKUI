aa0fd7be4c30bf7b382315e05bc6a42624b0bbb7
simon.weckler <simon.wecker@check24.de>
2024-05-24 19:55:35 +0200

    add button component

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Button from "~/Button";
+import { Search } from "~/icons";
+
+const meta = {
+  title: "Components/Atoms/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    disabled: false,
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Filled: Story = {
+  args: {
+    children: "Filled",
+    variant: "filled",
+  },
+};
+
+export const Outlined: Story = {
+  args: {
+    children: "Outlined",
+    variant: "outlined",
+  },
+};
+
+export const OutlinedPrimary: Story = {
+  args: {
+    children: "Outlined Primary",
+    variant: "outlined-primary",
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    children: "Button with Icon",
+    icon: <Search />,
+  },
+};
