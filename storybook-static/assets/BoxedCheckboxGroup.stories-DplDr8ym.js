41594f10dbe6bd320eeea08701d818faaa8f2ad6
simon.weckler <simon.wecker@check24.de>
2024-05-25 15:27:40 +0200

    add storybook static

---
+++ storybook-static/assets/BoxedCheckboxGroup.stories-DplDr8ym.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-BhBAkL08.js";import{B as h,C as a}from"./Checkbox-CtHW0Rtz.js";import{f as g}from"./index-Cq_GZlP2.js";import"./index-caxmlYbZ.js";import"./index-C56Ec2w9.js";import"./index-B4ZogDlv.js";import"./index-BmNgusFo.js";import"./index-CLI9pqPX.js";import"./utils-C-5SdYIA.js";import"./Search-BVt7rsam.js";import"./index-Bqoxw6Vv.js";const D={title:"Components/BoxedCheckboxGroup",component:h,parameters:{layout:"centered"},decorators:[u=>e.jsx("div",{className:"w-96",children:e.jsx(u,{})})],args:{label:"Label",onClickHelpIcon:g(),direction:"column",children:e.jsxs(e.Fragment,{children:[e.jsx(a,{value:"1",children:"Text 1"}),e.jsx(a,{value:"2",children:"Text 2"})]})},tags:["autodocs"]},r={},o={args:{error:"Error Text"}},s={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(a,{value:"1",children:"Text 1"}),e.jsx(a,{value:"2",disabled:!0,children:"Text 2"})]})}};var t,c,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(n=(c=r.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var i,l,m;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    error: "Error Text"
+  }
+}`,...(m=(l=o.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};var d,p,x;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <Checkbox value={"1"}>Text 1</Checkbox>
+        <Checkbox value={"2"} disabled>
+          Text 2
+        </Checkbox>
+      </>
+  }
+}`,...(x=(p=s.parameters)==null?void 0:p.docs)==null?void 0:x.source}}};const F=["Regular","Error","DisabledSingleElement"];export{s as DisabledSingleElement,o as Error,r as Regular,F as __namedExportsOrder,D as default};
---
+++ storybook-static/assets/CheckboxGroup.stories-Dr51Qup1.js
@@ -0,0 +1,10 @@
+import{j as e}from"./jsx-runtime-BhBAkL08.js";import{a as d,C as o}from"./Checkbox-CtHW0Rtz.js";import"./index-caxmlYbZ.js";import"./index-C56Ec2w9.js";import"./index-B4ZogDlv.js";import"./index-BmNgusFo.js";import"./index-CLI9pqPX.js";import"./utils-C-5SdYIA.js";import"./Search-BVt7rsam.js";const v={title:"Components/CheckboxGroup",component:d,parameters:{layout:"centered"},decorators:[m=>e.jsx("div",{className:"w-96",children:e.jsx(m,{})})],args:{direction:"column",children:e.jsxs(e.Fragment,{children:[e.jsx(o,{value:"1",children:"Text 1"}),e.jsx(o,{value:"2",children:"Text 2"})]})},tags:["autodocs"]},r={},s={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(o,{value:"1",children:"Text 1"}),e.jsx(o,{value:"2",disabled:!0,children:"Text 2"})]})}};var a,t,c;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"{}",...(c=(t=r.parameters)==null?void 0:t.docs)==null?void 0:c.source}}};var n,i,l;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <Checkbox value="1">Text 1</Checkbox>
+        <Checkbox value="2" disabled>
+          Text 2
+        </Checkbox>
+      </>
+  }
+}`,...(l=(i=s.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const T=["Regular","DisabledSingleElement"];export{s as DisabledSingleElement,r as Regular,T as __namedExportsOrder,v as default};
