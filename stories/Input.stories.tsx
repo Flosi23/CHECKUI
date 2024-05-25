f43bfea23f82c52e124bd9986fc7cd18a824ca28
paul.zhang <paul.zhang@check24.de>
2024-05-25 09:46:03 +0200

    Add Input

---
+++ stories/Input.stories.tsx
@@ -0,0 +1,75 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Input } from "@lib/Input";
+import { fn } from "@storybook/test";
+import { Edit } from "@lib/icons";
+
+const meta = {
+  title: "Components/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    label: "Vorname",
+    name: "firstName",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Regular: Story = {};
+
+export const Error: Story = {
+  args: {
+    error: "Error Text",
+  },
+};
+
+export const ErrorFilledOut: Story = {
+  args: {
+    error: "Error Text",
+    defaultValue: "Max",
+  },
+};
+
+export const DisabledEmpty: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const DisabledFilledOut: Story = {
+  args: {
+    defaultValue: "Max",
+    disabled: true,
+  },
+};
+
+export const HelpOverlay: Story = {
+  args: {
+    onClickHelpIcon: fn(),
+  },
+};
+
+export const HelpOverlayIcon: Story = {
+  args: {
+    onClickHelpIcon: fn(),
+    icon: Edit,
+  },
+};
+
+export const DisabledHelpOverlayIcon: Story = {
+  args: {
+    onClickHelpIcon: fn(),
+    icon: Edit,
+    disabled: true,
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    loading: true,
+  },
+};
